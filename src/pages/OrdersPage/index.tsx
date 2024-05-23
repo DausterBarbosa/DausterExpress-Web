e032a3e92db4dc9c8b3bae96b1f1a1e6c586a3a4
Dauster Barbosa <dausterbarbosa12@gmail.com>
2024-05-23 11:43:07 -0300

    tabela configurada

--- src/pages/OrdersPage/index.tsx
+++ src/pages/OrdersPage/index.tsx
@@ -2,22 +2,35 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import WatchLaterIcon from '@mui/icons-material/WatchLater';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import ErrorIcon from '@mui/icons-material/Error';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+import IconButton from '@mui/material/IconButton';
 
 import { styled } from '@mui/system';
 
 const OrdersPageContainer = styled('div')({
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: '#EEE',
+    width: '90vw',
     display: 'flex',
     flexDirection: 'column',
+    margin: '100px auto 0',
 });
 
 const StatusOrdersPageContainer = styled('div')({
-    marginTop: '100px',
     flexDirection: 'row',
     display: 'flex',
     alignItems: 'center',
@@ -35,6 +48,14 @@ const StatusContainer = styled('div')({
     '& + &': {
         marginLeft: '20px',
     }
+});
+
+const OrdersPageToolBarContainer = styled('div')({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: '30px 0'
 });
 
 export default function OrdersPage(){
@@ -70,6 +91,105 @@ export default function OrdersPage(){
                     </Box>
                 </StatusContainer>
             </StatusOrdersPageContainer>
+            <OrdersPageToolBarContainer>
+                <TextField sx={{backgroundColor: '#FFF'}} size='small' placeholder='Pesquisar encomenda'/>
+                <Stack spacing={2} direction="row">
+                    <div>
+                        <Button variant="contained" sx={{backgroundColor: '#4d148c', fontWeight: 'bold'}}>FILTRAR</Button>
+                         <Menu
+                            open={false}
+                        >
+                            <MenuItem>Profile</MenuItem>
+                            <MenuItem>My account</MenuItem>
+                            <MenuItem>Logout</MenuItem>
+                        </Menu>
+                    </div>
+                    <Button variant="contained" sx={{backgroundColor: '#4d148c', fontWeight: 'bold'}}>ADICIONAR</Button>
+                </Stack>
+            </OrdersPageToolBarContainer>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead sx={{backgroundColor: '#4d148c'}}>
+                        <TableRow>
+                            <TableCell align="center" sx={{fontWeight: 'bold', color: '#FFF', fontSize: '14px'}}>DESTINATÁRIO</TableCell>
+                            <TableCell align="center" sx={{fontWeight: 'bold', color: '#FFF', fontSize: '14px'}}>ENTREGADOR</TableCell>
+                            <TableCell align="center" sx={{fontWeight: 'bold', color: '#FFF', fontSize: '14px'}}>CIDADE</TableCell>
+                            <TableCell align="center" sx={{fontWeight: 'bold', color: '#FFF', fontSize: '14px'}}>ESTADO</TableCell>
+                            <TableCell align="center" sx={{fontWeight: 'bold', color: '#FFF', fontSize: '14px'}}>STATUS</TableCell>
+                            <TableCell align="center" sx={{fontWeight: 'bold', color: '#FFF', fontSize: '14px'}}>AÇÕES</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">
+                                <IconButton sx={{ padding: 0, margin: 0 }}>
+                                    <MoreVertIcon/>
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">
+                                <IconButton sx={{ padding: 0, margin: 0 }}>
+                                    <MoreVertIcon/>
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">
+                                <IconButton sx={{ padding: 0, margin: 0 }}>
+                                    <MoreVertIcon/>
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">
+                                <IconButton sx={{ padding: 0, margin: 0 }}>
+                                    <MoreVertIcon/>
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">asdfasdf</TableCell>
+                            <TableCell align="center">asdfasdfasdf</TableCell>
+                            <TableCell align="center">
+                                <IconButton sx={{ padding: 0, margin: 0 }}>
+                                    <MoreVertIcon/>
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+                <TablePagination
+                    count={10}
+                    onPageChange={() => {}}
+                    page={1}
+                    rowsPerPage={-1}
+                    rowsPerPageOptions={[]}
+                />
+            </TableContainer>
         </OrdersPageContainer>
     );
 }
